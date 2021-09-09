b82573d260d309be6dced86ec0a76c81e15d98e0
Albert Sanchez <50465984+AlbertSanchezMejia@users.noreply.github.com>
2021-09-09 01:07:57 -0500

    Proyecto de Unity, prototipo escenario y movimiento

---
+++ BiciJam Game/Assets/MovementAddforce.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementAddforce : MonoBehaviour
+{
+    public int movementSpeed;
+    Rigidbody rigidBody;
+    Vector3 movementDirection;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        Movement();
+    }
+    void Movement()
+    {
+        float xMovement = Input.GetAxis("Horizontal");
+        float zMovement = Input.GetAxis("Vertical");
+        movementDirection = new Vector3(xMovement * movementSpeed, rigidBody.velocity.y, zMovement * movementSpeed);
+        rigidBody.AddForce(movementDirection * movementSpeed * Time.deltaTime);
+
+    }
+}
