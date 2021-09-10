463b5f6909b75ad09a0046d9761382596feb85c5
Albert Sanchez <50465984+AlbertSanchezMejia@users.noreply.github.com>
2021-09-10 02:20:05 -0500

    Bicicleta Movimiento Prototipo Avanzado

---
+++ BiciJam Game/Assets/BicycleMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BicycleMovement : MonoBehaviour
+{
+    public int movementSpeed;
+    public int turnSpeed;
+    Rigidbody rigidBody;
+    Vector3 movementDirection;
+    private float rot = 0.0f;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+        Rotation();
+    }
+
+    void Movement()
+    {
+        float zMovement = Input.GetAxis("Vertical") * 10;
+        movementDirection = new Vector3(0, 0, zMovement);
+        rigidBody.AddRelativeForce(movementDirection * movementSpeed * Time.deltaTime);
+    }
+
+    void Rotation()
+    {
+        rot += Input.GetAxis("Horizontal") * turnSpeed;
+        transform.eulerAngles = new Vector3(0.0f, rot, 0.0f);
+    }
+
+}
---
+++ BiciJam Game/Assets/BikeMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BikeMovement : MonoBehaviour
+{
+    CharacterController controller;
+
+    public int movementSpeed;
+
+    float rot = 0.0f;
+    public int turnSpeed;
+
+    Vector3 movementDirection;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+        Rotation();
+        Gravity();
+    }
+
+    void Movement()
+    {
+        float zMovement = Input.GetAxis("Vertical");
+        controller.Move(transform.forward * zMovement * movementSpeed * Time.deltaTime);
+    }
+
+    void Rotation()
+    {
+        rot += Input.GetAxis("Horizontal") * turnSpeed;
+        transform.eulerAngles = new Vector3(0.0f, rot, 0.0f);
+    }
+    void Gravity()
+    {
+        movementDirection = new Vector3(0, movementDirection.y, 0);
+        movementDirection.y = movementDirection.y + (Physics.gravity.y * 1 * Time.deltaTime);
+        controller.Move(movementDirection * Time.deltaTime);
+    }
+
+}
